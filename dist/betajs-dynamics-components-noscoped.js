eb0c47655b233b7f46ee9db1d80e33ab7e565adf
Victor Lingenthal <victor.lingenthal@gmail.com>
2015-09-24 21:07:40 +0200

    U

--- dist/betajs-dynamics-components-noscoped.js
+++ dist/betajs-dynamics-components-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-dynamics-components - v0.0.3 - 2015-09-22
+betajs-dynamics-components - v0.0.3 - 2015-09-24
 Copyright (c) Oliver Friedmann
 MIT Software License.
 */
@@ -17,7 +17,7 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "5d9ab671-06b1-49d4-a0ea-9ff09f55a8b7",
-		version: '35.1442944954327'
+		version: '36.1443096623569'
 	};
 });
 
