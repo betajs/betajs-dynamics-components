5c4a826cad2ae30dab1884791aa773aed70f4a88
Victor Lingenthal <victor.lingenthal@gmail.com>
2017-05-17 18:33:44 +0900

    Simulator

---
+++ simulator/components.js
@@ -0,0 +1,46 @@
+var components = new BetaJS.Collections.Collection([
+
+    //Inputs
+    {value  : 'scrollpicker'},
+    {value  : 'textinput'},
+    {value  : 'clickinput'},
+
+    // List - Listitems
+    {value  : 'test_selectableitem'},
+    {value  : 'selectableitem'},
+    {value  : 'clickitem'},
+
+    //Web -
+    {value  : 'layout_web'},
+    {value  : 'menu_web'},
+    {value  : 'header'},
+
+    //Swipecontainers
+    {value  : 'hoverbuttoncontainer'},
+    {value  : 'swipeclickcontainer'},
+    {value  : 'eventitem'},
+
+    //Titledlist
+    {value  : 'addtitle'},
+    {value  : 'titledlist'},
+    {value  : 'test_titledlist'},
+    {value  : 'test_titledlist_swipe'},
+
+    //List
+    {value  : 'test_list_loadmore'},
+    {value  : 'list'},
+    {value  : 'test_list_clickitem'},
+    {value  : 'test_list_listoflist'},
+    {value  : 'test_list_swipecontainer'},
+    {value  : 'test_list_listcollection'},
+    {value  : 'test_list_pushfunc'},
+
+    //Titledlist
+    {value  : 'searchlist'},
+    {value  : 'test_searchlist'},
+
+    //{value  : 'addvalue'},
+    {value  : 'overlaycontainer'},
+    {value  : 'testoverlaycontainer'}
+
+]);
