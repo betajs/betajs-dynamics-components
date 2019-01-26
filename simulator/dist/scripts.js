00a2862ee4d87166dca72b0b13fd80cdf6f611af
Victor Lingenthal <victor.lingenthal@gmail.com>
2019-01-26 14:46:22 +0700

    U

--- simulator/dist/scripts.js
+++ simulator/dist/scripts.js
@@ -1,5 +1,5 @@
 /*!
-betajs-dynamics-components - v0.1.74 - 2018-11-27
+betajs-dynamics-components - v0.1.76 - 2019-01-26
 Copyright (c) Victor Lingenthal,Oliver Friedmann
 Apache-2.0 Software License.
 */
